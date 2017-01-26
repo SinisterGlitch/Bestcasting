2a25fe9855a002d988d2aa2931b54c4f6dfbdac9
Germain Riahi <g.riahi@bert.house>
2017-01-26 22:52:04 +0100

    Cleaned up webpack config and finished minimized build

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,28 +1,46 @@
+var webpack = require("webpack");
+
 module.exports = {
-    entry: './application/main.jsx',
+    entry: {
+        app: './application/main.jsx',
+        vendor: [
+            "react",
+            "react-addons-linked-state-mixin",
+            "react-bootstrap",
+            "react-dom",
+            "react-router",
+            "react-router-bootstrap",
+            "react-tap-event-plugin",
+            "reflux",
+            "superagent",
+            "history",
+            "lodash"
+        ]
+    },
     output: {
         path: __dirname + '/../web/',
         filename: 'js/app.js',
+        library: "React",
         devtoolModuleFilenameTemplate: '../ui/[resource-path]'
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin("vendor", "js/vendor.js"),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        })
+    ],
     module: {
         loaders: [{
-            test: /\.css$/,
-            loader: "style-loader!css-loader"
-        }, {
             test: /\.jsx?$/,
             exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            plugins: ["import", {
-                    libraryName: "antd",
-                    libraryDirectory: "lib",
-                    style: 'css'
-                }
-            ]
+            loader: 'babel-loader'
         }]
     },
     resolve: {
         modulesDirectories: ['node_modules', 'application'],
-        extensions: ['', '.js', '.jsx', '.json', '.css']
+        extensions: ['', '.js', '.jsx', '.json']
     }
 };
