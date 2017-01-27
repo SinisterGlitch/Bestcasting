66e5637a7c8dd1980b09a23b7a1f7dc4cde4cbe7
Germain Riahi <g.riahi@bert.house>
2017-01-27 11:07:51 +0100

    Fixed login features

--- ui/application/routes.jsx
+++ ui/application/routes.jsx
@@ -1,16 +1,16 @@
 'use strict';
 
 import React from 'react';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import App from 'components/layout/app';
 import Base from 'components/layout/base';
 
 // Auth
-import loginView from 'modules/views/dashboard/login';
-import registerView from 'modules/views/dashboard/register';
+import authNotFoundView from 'modules/views/auth/404';
+import authLoginView from 'modules/views/auth/login';
+import authRegisterView from 'modules/views/auth/register';
 
 // dashboard
-import notFoundView from 'modules/views/dashboard/not-found';
 import dashboardIndexView from 'modules/views/dashboard/index';
 
 // stores
@@ -40,34 +40,34 @@ import slidesNewView from 'modules/views/slides/new';
 export default (
     <Router history={browserHistory}>
         <Route component={Base} path="/">
-            <Route component={loginView} path="login" />
-            <Route component={registerView} path="register" />
-        </Route>
+            <Route component={authLoginView} path="login" />
+            <Route component={authRegisterView} path="register" />
 
-        <Route component={App} path="/dashboard">
-            <Route component={dashboardIndexView} path="/dashboard" />
+            <Route component={App}>
+                <IndexRoute component={dashboardIndexView} path="/dashboard" />
 
-            <Route component={storesListView} path="/stores" />
-            <Route component={storesDetailView} path="/stores/detail/:id" />
-            <Route component={storesEditView} path="/stores/edit/:id" />
-            <Route component={storesNewView} path="/stores/new" />
+                <Route component={storesListView} path="/stores" />
+                <Route component={storesDetailView} path="/stores/detail/:id" />
+                <Route component={storesEditView} path="/stores/edit/:id" />
+                <Route component={storesNewView} path="/stores/new" />
 
-            <Route component={screensListView} path="/screens" />
-            <Route component={screensDetailView} path="/screens/detail/:id" />
-            <Route component={screensEditView} path="/screens/edit/:id" />
-            <Route component={screensNewView} path="/screens/new" />
+                <Route component={screensListView} path="/screens" />
+                <Route component={screensDetailView} path="/screens/detail/:id" />
+                <Route component={screensEditView} path="/screens/edit/:id" />
+                <Route component={screensNewView} path="/screens/new" />
 
-            <Route component={playlistsListView} path="/playlists" />
-            <Route component={playlistsDetailView} path="/playlists/detail/:id" />
-            <Route component={playlistsEditView} path="/playlists/edit/:id" />
-            <Route component={playlistsNewView} path="/playlists/new" />
+                <Route component={playlistsListView} path="/playlists" />
+                <Route component={playlistsDetailView} path="/playlists/detail/:id" />
+                <Route component={playlistsEditView} path="/playlists/edit/:id" />
+                <Route component={playlistsNewView} path="/playlists/new" />
 
-            <Route component={slidesListView} path="/slides" />
-            <Route component={slidesDetailView} path="/slides/detail/:id" />
-            <Route component={slidesEditView} path="/slides/edit/:id" />
-            <Route component={slidesNewView} path="/slides/new" />
+                <Route component={slidesListView} path="/slides" />
+                <Route component={slidesDetailView} path="/slides/detail/:id" />
+                <Route component={slidesEditView} path="/slides/edit/:id" />
+                <Route component={slidesNewView} path="/slides/new" />
 
-            <Route path="*" component={notFoundView}/>
+                <Route path="*" component={authNotFoundView}/>
+            </Route>
         </Route>
     </Router>
 );
